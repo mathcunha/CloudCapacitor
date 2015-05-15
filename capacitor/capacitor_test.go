af70e1e38910bf2d6121c1951b7cbd68db839aa8
Matheus Cunha <mathcunha@gmail.com>
2015-05-15 18:05:23 +0000

    testing shortest path

--- capacitor/capacitor_test.go
+++ capacitor/capacitor_test.go
@@ -29,6 +29,40 @@ func TestMarkStrict(t *testing.T) {
 	}
 }
 
+func TestMarkMemShortestPath(t *testing.T) {
+	vms, err := LoadTypes("/home/vagrant/go/src/github.com/mathcunha/CloudCapacitor/config/dspace.yml")
+	if err != nil {
+		t.Errorf("config error")
+	}
+	d := NewDeploymentSpace(&vms, 7.0, 4)
+	m := MockExecutor{"/home/vagrant/go/src/github.com/mathcunha/CloudCapacitor/config/wordpress_cpu_mem.csv", nil}
+	err = m.Load()
+	if err != nil {
+		t.Errorf("config error")
+	}
+	c := Capacitor{d, m}
+	mode := "Mem"
+	dspace := c.Dspace.CapacityBy(mode)
+	cat := "c3"
+
+	nodesInfo := buildMatrix([]string{"100", "200", "300", "400", "500", "600", "700", "800", "900", "1000"}, (*dspace)[cat])
+	//keys := []string{"3_c3_2xlarge#4", "3_c3_large#3", "2_c3_large#2", "4_c3_2xlarge#5", "1_c3_large#1", "1_c3_large#0", "4_c3_2xlarge#4"}
+	keys := []string{"3_c3_2xlarge#4", "3_c3_large#3"}
+	execs := 0
+	for _, key := range keys {
+		execs++
+		nodeInfo := nodesInfo.Matrix[key]
+		result := c.Executor.Execute(*nodeInfo.Config, nodeInfo.WKL)
+		(&nodesInfo).Mark(key, result.SLO <= 10000, execs)
+	}
+
+	t.Logf("%v - BEFORE KEY[2_c3_large#2]", c.NodesLeft(&nodesInfo))
+	nodeInfo := nodesInfo.Matrix["2_c3_large#2"]
+	result := c.Executor.Execute(*nodeInfo.Config, nodeInfo.WKL)
+	(&nodesInfo).Mark("2_c3_large#2", result.SLO <= 10000, execs)
+	t.Logf("%v - AFTER KEY[2_c3_large#2]", c.NodesLeft(&nodesInfo))
+}
+
 func TestMarkMem(t *testing.T) {
 	vms, err := LoadTypes("/home/vagrant/go/src/github.com/mathcunha/CloudCapacitor/config/dspace.yml")
 	if err != nil {
