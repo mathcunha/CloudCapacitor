b30b379a4a9bdad583e1edf357ba47ab1521f8ab
Matheus Cunha <mathcunha@gmail.com>
2015-10-26 14:06:25 +0000

    not logging DSpace SVG

--- webui/static/js/capacitor.js
+++ webui/static/js/capacitor.js
@@ -104,7 +104,6 @@
 						else{
 							showSuccessMessage('Done!')
 							var svg = Viz(data, "svg");
-							console.log(svg)
 							panel = '<div class=\"alert alert-info alert-dismissible\" role=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button><hr>'+svg;
 							$("#mainAlertPanel").append(panel);
 						}
